3bb099350027de743b8c432c00a7f67c3648129c
md-maruf-hossain <marufhossain.6470@gmail.com>
2024-09-21 16:44:56 +0600

    nav created

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -2,30 +2,30 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer bg-green-600 text-white items-center p-4">
-      <aside className="grid-flow-col items-center font-semibold text-">
-        <p>A M Almas Shahriyar Azad-</p>
-        <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+    <footer className="bg-green-600 text-white p-4 flex flex-col md:flex-row md:justify-between items-center">
+      <aside className="text-center md:text-left font-semibold mb-2 md:mb-0">
+        <p>A M Almas Shahriyar Azad</p>
+        <p>Copyright © {new Date().getFullYear()} - All rights reserved</p>
       </aside>
-      <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end font-semibold">
+      <nav className="flex flex-wrap justify-center md:justify-end gap-4 font-semibold">
         {/* Email */}
-        <a href="mailto:aaaarnab1234@gmail.com" title="Email" rel="noopener noreferrer" className=" hover:underline">
+        <a href="mailto:aaaarnab1234@gmail.com" title="Email" rel="noopener noreferrer" className="hover:underline">
           Email
         </a>
         {/* Google Scholar */}
-        <a href="https://scholar.google.com/citations?user=YSeH6mUAAAAJ&hl=en" target="_blank" rel="noopener noreferrer" className=" hover:underline">
+        <a href="https://scholar.google.com/citations?user=YSeH6mUAAAAJ&hl=en" target="_blank" rel="noopener noreferrer" className="hover:underline">
           Google Scholar
         </a>
         {/* ResearchGate */}
-        <a href="https://www.researchgate.net/profile/A-M-Azad" target="_blank" rel="noopener noreferrer" className=" hover:underline">
+        <a href="https://www.researchgate.net/profile/A-M-Azad" target="_blank" rel="noopener noreferrer" className="hover:underline">
           ResearchGate
         </a>
         {/* LinkedIn */}
-        <a href="https://bd.linkedin.com/in/a-m-almas-shahriyar-azad-csca%E2%84%A2-40559a1b1 " target="_blank" rel="noopener noreferrer" className=" hover:underline">
+        <a href="https://bd.linkedin.com/in/a-m-almas-shahriyar-azad-csca%E2%84%A2-40559a1b1" target="_blank" rel="noopener noreferrer" className="hover:underline">
           LinkedIn
         </a>
         {/* Facebook */}
-        <a href="https://www.facebook.com/aristocratic.arnab" target="_blank" rel="noopener noreferrer" className=" hover:underline">
+        <a href="https://www.facebook.com/aristocratic.arnab" target="_blank" rel="noopener noreferrer" className="hover:underline">
           Facebook
         </a>
       </nav>
--- src/Pages/Shared/Nav/Nav.jsx
+++ src/Pages/Shared/Nav/Nav.jsx
@@ -1,11 +1,107 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
-    return (
-        <div>
-            nav
+  return (
+    <div className="navbar bg-green-600">
+      <div className="navbar">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
+          </div>
+          <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow text-green-600 font-semibold">
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Education</a>
+            </li>
+            <li>
+              <a>Research Profile & Publications</a>
+            </li>
+            <li>
+              <a>Current Research Projects</a>
+            </li>
+            <li>
+              <a>Current Research Initiatives</a>
+            </li>
+            <li>
+              <a>Academic Projects</a>
+            </li>
+            <li>
+              <a>Peer Review Experience</a>
+            </li>
+            <li>
+              <a>Training & Certification </a>
+            </li>
+            <li>
+              <a>Others</a>
+              <ul className="p-2">
+                <li>
+                  <a>Family</a>
+                </li>
+                <li>
+                  <a>Hobbies</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
         </div>
-    );
+        <Link to="/" className="text-xl text-white font-semibold">A M Almas Shahriyar Azad</Link>
+      </div>
+      {/* pc */}
+      <div className="navbar-center hidden lg:flex text-white font-semibold">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>Education</a>
+          </li>
+          <li>
+            <details>
+              <summary>Research</summary>
+              <ul className="p-2 text-green-600">
+                <li>
+                  <a>Research Profile & Publications</a>
+                </li>
+                <li>
+                  <a>Current Research Projects</a>
+                </li>
+                <li>
+                  <a>Current Research Initiatives</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <a>Academic Projects</a>
+          </li>
+          <li>
+            <a>Peer Review Experience</a>
+          </li>
+          <li>
+            <a>Training & Certification </a>
+          </li>
+          <li>
+            <details>
+              <summary>Others</summary>
+              <ul className="p-2">
+                <li>
+                  <a>Family</a>
+                </li>
+                <li>
+                  <a>Hobby</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
